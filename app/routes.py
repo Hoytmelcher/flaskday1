c8192236df390a678a5642b92cf0474d74c2b1b0
Hoytmelcher <122131937+Hoytmelcher@users.noreply.github.com>
2023-03-01 06:26:18 -0600

    added messages and redirect

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import RegisterForm, LoginForm, CarForm
 
@@ -10,21 +10,27 @@ def index():
 def about():
     return render_template('about.jinja')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        flash(f'Request to register {form.username} sucessful')
+        return redirect('/')
     return render_template('register.jinja', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Welcome back')
+        return redirect('/')
     return render_template('login.jinja', form=form)
 
 @app.route('/blog')
 def blog():
     return render_template('blog.jinja')
 
-@app.route('/cars')
+@app.route('/cars', methods=['GET', 'POST'])
 def cars():
     form = CarForm()
     return render_template('cars.jinja', form=form)
